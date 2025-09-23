29ef06fe33ac7bf35c4318cf730e3af6638d440e
ryszhio <karkirishab78@gmail.com>
2025-09-23 18:20:48 +0545

    fix: attached vercel.json to fix issue with react-routing

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/projects" element={<Projects/>}/>
       </Routes>
-    </BrowserRouter>
+   </BrowserRouter>
   )
 }
 
